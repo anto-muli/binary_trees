037d736d24dc86867308279059338eae1bc92e59
anto-muli <memuli@students.uonbi.ac.ke>
2024-02-01 22:00:15 +0300

    done

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -8,8 +8,8 @@ void pop(levelorder_queue_t **head);
 int binary_tree_is_complete(const binary_tree_t *tree);
 
 /**
- * create_node - Creates a new levelorder_queue_t node.
- * @node: The binary tree node for the new node to contain.
+ * create_node - creates a new levelorder_queue_t node.
+ * @node: The bin tree node for the new node to contain.
  *
  * Return: If an error occurs, NULL.
  *         Otherwise, a pointer to the new node.
@@ -29,8 +29,8 @@ levelorder_queue_t *create_node(binary_tree_t *node)
 }
 
 /**
- * free_queue - Frees a levelorder_queue_t queue.
- * @head: A pointer to the head of the queue.
+ * free_queue - removes a levelorder_queue_t queue.
+ * @head: This is a ptr to the head of the queue.
  */
 void free_queue(levelorder_queue_t *head)
 {
@@ -82,7 +82,7 @@ void pop(levelorder_queue_t **head)
 
 /**
  * binary_tree_is_complete - Checks if a binary tree is complete.
- * @tree: A pter to the root node of the tree to traverse.
+ * @tree: A pte to the root node of the tree to traverse.
  *
  * Return: If the tree is NULL or not complete, 0.
  *         Otherwise, 1.
